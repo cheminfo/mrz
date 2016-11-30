2fffab651ec98bac3c4f5e0fe74ec6570225842a
lpatiny <gb252243>
2016-11-30 07:53:13 +0100

    Rename to parse

--- src/index.js
+++ src/index.js
@@ -5,11 +5,7 @@ const COUNTRIES = require('./util/countries');
 const parse = require('./parse/parse');
 
 
-function parseMRZ(text) {
-    return parse(text);
-}
-
 module.exports = {
     COUNTRIES,
-    parseMRZ
+    parse
 };
