9238752cae2d9fba96e6d218c20580073018e678
Luc Patiny <luc@patiny.com>
2018-01-16 15:46:29 +0100

    fix:  documentNumber in TD1 is in optional field

--- src/util/parseDocumentNumber.js
+++ src/util/parseDocumentNumber.js
@@ -10,15 +10,16 @@ var check = require('./check');
 module.exports = function parseDocumentNumber(source, checkDigit, optional) {
   if (checkDigit === '<' && optional) {
     optional = optional.replace(/<.*/, '');
-    source += optional.substring(0, optional.length - 1);
+    source += checkDigit + optional.substring(0, optional.length - 1);
     checkDigit = optional.charAt(optional.length - 1);
   }
   var result = {
     source,
     label: 'Document number',
-    value: source.replace(/<*$/, ''),
+    value: source.replace(/</g, ''),
     error: []
   };
+
   if (!check(source, checkDigit)) {
     result.error.push(`Check digit "${checkDigit}" not valid`);
   }
