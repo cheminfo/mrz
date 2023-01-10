bc42f5c7e647df256c1e6df5cb93c70cf55be53f
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2023-01-10 21:39:13 +0100

    refactor: fix used link

--- src/parse/frenchNationalIdCorrection.ts
+++ src/parse/frenchNationalIdCorrection.ts
@@ -2,8 +2,7 @@
 
 import { getInterval } from './utils/getInterval';
 
-// based on https://en.wikipedia.org/wiki/National_identity_card_(France)
-// TODO: this ressource must be confirmed
+// based on https://fr.wikipedia.org/wiki/Carte_nationale_d%27identit%C3%A9_en_France#Codage_Bande_MRZ_(lecture_optique)
 
 const lettersOnly = [[...getInterval(1, 30)], [...getInterval(14, 27), 35]];
 const numbersOnly = [
