7e817e3774355638c6a88ae899fab3aba8816f79
Wadjih Bencheikh <jw_bencheikh@esi.dz>
2023-01-25 18:31:59 +0100

    refactor: remove useless files (#35)

--- src/parse/utils/__tests__/getInterval.test.ts
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-import { getInterval } from '../getInterval';
-
-test('check getInterval', () => {
-  const interval = getInterval(2, 6);
-  expect(interval).toEqual([2, 3, 4, 5, 6]);
-});
