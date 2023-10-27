d9c20141397c842147e8e47376dc007ccdd414cc
josoriom <josoriom93@gmail.com>
2023-10-26 19:03:28 -0500

    test: refactor autocorrect test to display value and corrections + test case

--- src/parse/__tests__/swissDrivingLicense.test.ts
+++ src/parse/__tests__/swissDrivingLicense.test.ts
@@ -33,9 +33,9 @@ describe('parse Swiss Driving License', () => {
       ranges: [
         {
           line: 2,
-          start: 0,
-          end: 30,
-          raw: 'MARCHAND<<FABIENNE<<<<<<<<<<<<',
+          start: 10,
+          end: 18,
+          raw: 'FABIENNE',
         },
       ],
       line: 2,
@@ -55,7 +55,7 @@ describe('parse Swiss Driving License', () => {
       lastName: 'MARCHAND',
     });
   });
-  it('Use autocorrect', () => {
+  it('Use autocorrect 1', () => {
     const MRZ = [
       'AAA001D<<',
       'FACHE305142128097<<800126<<<<<',
@@ -72,31 +72,97 @@ describe('parse Swiss Driving License', () => {
 
     expect(result.fields).toStrictEqual(correctedResult.fields);
     expect(
-      correctedResult.details.map(({ autocorrect }) => autocorrect),
+      correctedResult.details.map(({ value, autocorrect }) => ({
+        value,
+        autocorrect,
+      })),
     ).toStrictEqual([
-      [],
-      [],
-      [],
-      [],
-      [
-        { line: 1, column: 7, original: 'S', corrected: '5' },
-        { line: 1, column: 11, original: 'I', corrected: '1' },
-        { line: 1, column: 12, original: 'Z', corrected: '2' },
-        { line: 1, column: 13, original: 'B', corrected: '8' },
-      ],
-      [{ line: 1, column: 14, original: 'O', corrected: '0' }],
-      [],
-      [
-        { line: 1, column: 20, original: 'O', corrected: '0' },
-        { line: 1, column: 21, original: 'O', corrected: '0' },
-        { line: 1, column: 24, original: 'G', corrected: '6' },
-      ],
-      [],
-      [
-        { line: 2, column: 12, original: '8', corrected: 'B' },
-        { line: 2, column: 13, original: '1', corrected: 'I' },
-      ],
-      [],
+      { value: 'AAA001D', autocorrect: [] },
+      { value: 'D', autocorrect: [] },
+      { value: 'FA', autocorrect: [] },
+      { value: 'CHE', autocorrect: [] },
+      {
+        value: '305142128',
+        autocorrect: [
+          { line: 1, column: 7, original: 'S', corrected: '5' },
+          { line: 1, column: 11, original: 'I', corrected: '1' },
+          { line: 1, column: 12, original: 'Z', corrected: '2' },
+          { line: 1, column: 13, original: 'B', corrected: '8' },
+        ],
+      },
+      {
+        value: '097',
+        autocorrect: [{ line: 1, column: 14, original: 'O', corrected: '0' }],
+      },
+      { value: '<<', autocorrect: [] },
+      {
+        value: '800126',
+        autocorrect: [
+          { line: 1, column: 20, original: 'O', corrected: '0' },
+          { line: 1, column: 21, original: 'O', corrected: '0' },
+          { line: 1, column: 24, original: 'G', corrected: '6' },
+        ],
+      },
+      { value: '<<<<<', autocorrect: [] },
+      {
+        value: 'MARCHAND',
+        autocorrect: [],
+      },
+      {
+        value: 'FABIENNE',
+        autocorrect: [
+          { line: 2, column: 12, original: '8', corrected: 'B' },
+          { line: 2, column: 13, original: '1', corrected: 'I' },
+        ],
+      },
+    ]);
+  });
+
+  it('Use autocorrect 2', () => {
+    const MRZ = [
+      'AKU735D<<',
+      'FACHE000231268003<<530727<<<<<',
+      'AELLEN<<ROLAND<<<<<<<<<<<<<<<<',
+    ];
+    const falseMRZ = [
+      'AKU735D<<',
+      'FACHE000231268OO3<<53O727<<<<<',
+      'AELLEN<<R0LAND<<<<<<<<<<<<<<<<',
+    ];
+
+    const result = parse(MRZ);
+    const correctedResult = parse(falseMRZ, { autocorrect: true });
+    const corrections = correctedResult.details.map(
+      ({ value, autocorrect }) => ({ value, autocorrect }),
+    );
+    expect(result.fields).toStrictEqual(correctedResult.fields);
+    expect(corrections).toStrictEqual([
+      { value: 'AKU735D', autocorrect: [] },
+      { value: 'D', autocorrect: [] },
+      { value: 'FA', autocorrect: [] },
+      { value: 'CHE', autocorrect: [] },
+      { value: '000231268', autocorrect: [] },
+      {
+        value: '003',
+        autocorrect: [
+          { line: 1, column: 14, original: 'O', corrected: '0' },
+          { line: 1, column: 15, original: 'O', corrected: '0' },
+        ],
+      },
+      { value: '<<', autocorrect: [] },
+      {
+        value: '530727',
+        autocorrect: [{ line: 1, column: 21, original: 'O', corrected: '0' }],
+      },
+      { value: '<<<<<', autocorrect: [] },
+      {
+        value: 'AELLEN',
+        autocorrect: [],
+      },
+      {
+        value: 'ROLAND',
+        autocorrect: [{ line: 2, column: 9, original: '0', corrected: 'O' }],
+      },
     ]);
   });
 });
