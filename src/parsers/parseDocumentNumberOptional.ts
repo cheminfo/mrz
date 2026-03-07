dd6da8fce92cf9611a857eecd220d92bee3aadc4
Daniel Kostro <kostro.d@gmail.com>
2026-03-07 12:53:54 +0100

    tests: remove dead code and add a test case

--- src/parsers/parseDocumentNumberOptional.ts
+++ src/parsers/parseDocumentNumberOptional.ts
@@ -1,23 +1,10 @@
 import { parseText } from './parseText.ts';
 
-export function parseDocumentNumberOptional(
-  optional: string,
-  checkDigit: string,
-) {
-  if (checkDigit === '<') {
-    const firstFiller = optional.indexOf('<');
-    const value = parseText(optional.slice(firstFiller + 1), firstFiller + 1);
-    return {
-      value,
-      start: firstFiller + 1,
-      end: firstFiller + 1 + value.length,
-    };
-  } else {
-    const value = parseText(optional, 0);
-    return {
-      value,
-      start: 0,
-      end: value.length,
-    };
-  }
+export function parseDocumentNumberOptional(optional: string) {
+  const value = parseText(optional, 0);
+  return {
+    value,
+    start: 0,
+    end: value.length,
+  };
 }
