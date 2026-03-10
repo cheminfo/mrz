5dcc808514ba77c4fa022df5a85eb858def08141
Daniel Kostro <kostro.d@gmail.com>
2026-03-10 11:33:14 +0100

    tests: clarify TD1 with document number check digit in optional part
    Add tests, references, and clarify in comments.
    
    Expected check digit error based on document number input without the '<' character

--- src/parsers/__tests__/check.test.ts
+++ src/parsers/__tests__/check.test.ts
@@ -12,7 +12,32 @@ test('check digits', () => {
   expect(() => check('592166111<773', 4)).toThrow(/invalid check digit/);
 });
 
-test('compute embedded TD1 check digit', () => {
-  expect(computeCheckDigit('123456789AAB')).toBe(8);
-  expect(computeCheckDigit('123456789<AAB')).toBe(4);
+test('compute embedded TD1 check digit - undetermined', () => {
+  // https://www.consilium.europa.eu/prado/en/PRT-BO-04001/index.html
+  // I<PRT007666667<ZZ00<<<<<<<<<<<
+  expect(computeCheckDigit('007666667<ZZ0')).toBe(0);
+  expect(computeCheckDigit('007666667ZZ0')).toBe(0);
+});
+
+test('compute embedded TD1 check digit - must include < character', () => {
+  // https://www.consilium.europa.eu/prado/en/BEL-BO-03003/index.html
+  // IDBEL000590240<6013<<<<<<<<<<<
+  expect(computeCheckDigit('000590240<601')).toBe(3);
+  expect(computeCheckDigit('000590240601')).not.toBe(3);
+});
+
+test('compute embedded TD1 check digit - must not include < character', () => {
+  // https://www.consilium.europa.eu/prado/en/BEL-BO-11005/index.html
+  // IDBEL600001795<0152<<<<<<<<<<<
+  expect(computeCheckDigit('600001795015')).toBe(2);
+  expect(computeCheckDigit('600001795<015')).not.toBe(2);
+});
+
+test('embedded check digit - ICAO', () => {
+  // https://www.icao.int/sites/default/files/publications/DocSeries/9303_p11_cons_en.pdf
+  // Page 88
+  // I<UTOD23145890<7349<<<<<<<<<<<
+  // The ICAO document says it uses the number without the '<' character, but both yield the same result.
+  expect(computeCheckDigit('D23145890734')).toBe(9);
+  expect(computeCheckDigit('D23145890<734')).toBe(9);
 });
